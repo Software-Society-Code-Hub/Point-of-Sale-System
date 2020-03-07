904eed908d00daa280409ab02db80152f249f521
Leindfraust <lionheart023@yahoo.com>
2020-03-07 14:59:22 +0800

    still in progress
    creating Create User Account function

---
+++ Form2.vb
@@ -0,0 +1,18 @@
+﻿Imports System.IO
+Imports System.Data.OleDb
+Public Class Form2
+    Dim provider As String
+    Dim dataFile As String
+    Dim conString As String
+    Dim myConnection As OleDbConnection = New OleDbConnection
+
+    Private Sub Button1_Click(sender As Object, e As EventArgs) Handles Button1.Click
+        Dim userPrompt As String = TextBox1.Text
+        Dim passPrompt As String = TextBox2.Text
+        provider = "Provider=Microsoft.ACE.OLEDB.12.0; Data Source="
+        dataFile = "../../db/userDB.accdb"
+        conString = provider & dataFile
+        myConnection.ConnectionString = conString
+        myConnection.Open()
+    End Sub
+End Class
