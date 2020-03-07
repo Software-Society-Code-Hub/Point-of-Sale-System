b8bb32529d92963917c062413ddf45ca860d5c85
Leindfraust <lionheart023@yahoo.com>
2020-03-07 14:58:55 +0800

    Update Form1.vb
    added connection string, username and password check up

--- Form1.vb
+++ Form1.vb
@@ -1,3 +1,46 @@
-﻿Public Class Form1
+﻿Imports System.IO
+Imports System.Data.OleDb
+Public Class Form1
+    Public currentUser As String
+    Public defaultUser As String = "admin"
+    Public defaultPass As String = "admin"
 
+    Dim provider As String
+    Dim dataFile As String
+    Dim conString As String
+    Dim myConnection As OleDbConnection = New OleDbConnection
+
+    Private Sub Button1_Click(sender As Object, e As EventArgs) Handles Button1.Click
+        Dim userPrompt As String = TextBox1.Text
+        Dim passPrompt As String = TextBox2.Text
+        provider = "Provider=Microsoft.ACE.OLEDB.12.0; Data Source="
+        dataFile = "../../db/userDB.accdb"
+        conString = provider & dataFile
+        myConnection.ConnectionString = conString
+        myConnection.Open()
+        Dim cmd As OleDbCommand = New OleDbCommand("Select * FROM [userList] WHERE [username] = '" & TextBox1.Text & "' AND [password] = '" & TextBox2.Text & "' ", myConnection)
+        Dim dr As OleDbDataReader = cmd.ExecuteReader
+
+        Dim userDB As String = ""
+        Dim passDB As String = ""
+        While dr.Read
+            userDB = dr("username").ToString
+            passDB = dr("password").ToString
+        End While
+        myConnection.Close()
+
+        If userPrompt = defaultUser And passPrompt = defaultPass Then
+            currentUser = defaultUser
+            MsgBox("Hello " + currentUser)
+        ElseIf userPrompt = userDB And passPrompt = passDB Then
+            currentUser = userDB
+            MsgBox("Hello " + currentUser)
+        Else
+            MsgBox("invalid credentials")
+        End If
+    End Sub
+
+    Private Sub Button3_Click(sender As Object, e As EventArgs) Handles Button3.Click
+        Form2.Show()
+    End Sub
 End Class
