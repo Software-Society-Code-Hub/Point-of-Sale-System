856add3ce2a5d31b35eeca3013ed685727c551bb
Leindfraust <lionheart023@yahoo.com>
2020-03-08 13:33:59 +0800

    Create Form3.vb

---
+++ Form3.vb
@@ -0,0 +1,17 @@
+﻿Imports System.IO
+Imports System.Data.OleDb
+Public Class Form3
+    Private Sub Form1_Load(ByVal sender As System.Object, ByVal e As System.EventArgs) Handles MyBase.Load
+        Me.WindowState = FormWindowState.Maximized
+    End Sub
+
+    Public currentUser As String = Form1.currentUser
+    Public currentPriv As String = Form1.currentPriv
+
+    Dim provider As String
+    Dim dataFile As String
+    Dim conString As String
+    Dim myConnection As OleDbConnection = New OleDbConnection
+
+
+End Class
